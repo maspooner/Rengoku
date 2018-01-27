c8af2b473f0f48f4ba2b09ce19262e52084b2167
Matthew Spooner <spoonerm12@gmail.com>
2018-01-27 00:55:11 -0500

    Created build script for making the main game

---
+++ build.bat
@@ -0,0 +1,11 @@
+mkdir .\bin
+cd bin
+"C:\Program Files\Java\jdk1.8.0_131\bin\jar.exe" xfv ..\MRL.jar org
+cd ..
+"C:\Program Files\Java\jdk1.8.0_131\bin\javac.exe" -d ./bin -cp src;MRL.jar src/org/spooner/java/TileGame/*.java
+if %errorlevel% NEQ 0 (goto done)
+"C:\Program Files\Java\jdk1.8.0_131\bin\jar.exe" cvfm ./out/Rengoku.jar ./MANIFEST.MF -C bin org assets
+xcopy /i /y .\joy .\out\joy
+xcopy /i /y .\run.bat .\out\run.bat
+:done
+PAUSE
---
+++ run.bat
@@ -0,0 +1,1 @@
+java -jar Rengoku.jar
